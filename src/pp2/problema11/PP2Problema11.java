c4aab61b9ff52b0cbed203332033ff0c23d4f4c7
PC <PC@192.168.1.70>
2016-10-17 22:04:48 -0500

    11.	Realizar un programa que convierta un número dado a número romano

--- src/pp2/problema11/PP2Problema11.java
+++ src/pp2/problema11/PP2Problema11.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema11;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -15,7 +15,28 @@ public class PP2Problema11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int num= solicitarDatos("cantidad de numeros en el numero ");
+        int[] numero = new int[num];
+       
     }
-    
+    public static int solicitarDatos(String d){ //Solicita datos
+        int miDato;
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.print("Introduce el dato " + d);
+        miDato= entradaEscaner.nextInt();
+        return miDato;
+    }
+     public static int [] llenarVector(int []n){//Metodo de llenar el vector, recibe un arreglo de enteros, regresa un arreglo de enteros
+        Scanner entradaEscaner = new Scanner(System.in);       
+        for (int i = 0; i<n.length; i++){//Mientras que el valor de i sea menor que el tamaño del arreglo(10), i se aumentara en 1
+            System.out.print("Introduce un número para la posición [" + i + "]"); //i indica la posición
+            n[i]=entradaEscaner.nextInt();//En la pocision i se asigna el valor
+                                          //Y regresa hasta que la posicion i se vuelva 10
+                                          //En ese momento regresa el arreglo
+        }
+        return n;
+       }
+    public static void c(){
+        
+    }
 }
